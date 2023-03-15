9dcd04d33eaa5febec18b6555947a74bb0ef5a33
Irina Agliullina <Agliullina2006@yandex.ru>
2023-03-15 13:03:39 +0400

    Исправлена запись arr.push()

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,13 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = b ** 2 - 4 * a * c;
+  const d = b**2 - 4*a*c;
 
   if (d > 0) {
-    arr.push(-b + Math.sqrt(d) ) / (2 * a);
-    arr.push(-b - Math.sqrt(d) ) / (2 * a);
+    arr.push( (-b + Math.sqrt(d) )/(2*a) );
+    arr.push( (-b - Math.sqrt(d) )/(2*a) );
   } else if (d === 0); {
-    arr.push(-b / (2 * a) );
+    arr.push(-b / (2*a) );
   }; 
   return arr;
 }
