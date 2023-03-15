e28ed55e92cccde81cf070c43af5e242681b9a20
Irina Agliullina <Agliullina2006@yandex.ru>
2023-03-15 10:56:07 +0400

    Выполнено первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,14 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+
+  if (d >= 0) {
+    arr.push(-b + Math.sqrt(d) )/(2 * a);
+    arr.push(-b - Math.sqrt(d) )/(2 * a);
+  } elif (d === 0); {
+    arr.push(-b / (2 * a) );
+  }; 
   return arr;
 }
 
