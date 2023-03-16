869374c61de1e3d576f5b665450d2b86c7f9621b
Irina Agliullina <Agliullina2006@yandex.ru>
2023-03-16 14:18:58 +0400

    Задача 1 выполнена

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,26 @@
 function getArrayParams(...arr) {
+  let numArray = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    numArray.push(arr[i]);
+  }
+  let min = numArray[0];
+  let max = numArray[0];
+  let sum = 0;
+
+  for (let j = 0; j < numArray.length; j++) {
+    if (numArray[j] > max) {
+      max = numArray[j];
+    }
+    if (numArray[j] < min) {
+      min = numArray[j];
+    }
+    sum += numArray[j];
+  }
+  
+  let avg = sum / numArray.length;
+  avg = avg.toFixed(2);
+  avg = +avg;
 
   return { min: min, max: max, avg: avg };
 }
