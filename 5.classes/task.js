2a30255f6b69b898e584a85ab5c7d129abb8eefd
Irina Agliullina <Agliullina2006@yandex.ru>
2023-03-27 16:48:20 +0400

    Задача 1, исправлен порядок аргументов в конструкторах книг

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -27,14 +27,14 @@ class PrintEditionItem {
 }
 
 class Magazine extends PrintEditionItem {
-  constructor() {
-    super();
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
     this.type = 'magazine';
   }
 }
 
 class Book extends PrintEditionItem {
-  constructor(name, releaseDate, pagesCount, author) {
+  constructor(author, name, releaseDate, pagesCount) {
     super(name, releaseDate, pagesCount);
     this.author = author;
     this.type = 'book';
@@ -42,21 +42,21 @@ class Book extends PrintEditionItem {
 }
 
 class NovelBook extends Book {
-  constructor(name, releaseDate, pagesCount, author) {
+  constructor(author, name, releaseDate, pagesCount) {
     super(name, releaseDate, pagesCount, author);
     this.type = 'novel';
   }
 }
 
 class FantasticBook extends Book {
-  constructor(name, releaseDate, pagesCount, author) {
+  constructor(author, name, releaseDate, pagesCount, author) {
     super(name, releaseDate, pagesCount, author);
     this.type = 'fantastic';
   }
 }
 
 class DetectiveBook extends Book {
-  constructor(name, releaseDate, pagesCount, author) {
+  constructor(author, name, releaseDate, pagesCount, author) {
     super(name, releaseDate, pagesCount, author);
     this.type = 'detective';
   }
