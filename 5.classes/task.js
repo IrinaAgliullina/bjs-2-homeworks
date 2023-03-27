d7a251125ee4e1510a2e7a46a2222b23b8e2a218
Irina Agliullina <Agliullina2006@yandex.ru>
2023-03-27 16:58:25 +0400

    test

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -35,7 +35,7 @@ class Magazine extends PrintEditionItem {
 
 class Book extends PrintEditionItem {
   constructor(author, name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
+    super(author, name, releaseDate, pagesCount);
     this.author = author;
     this.type = 'book';
   }
@@ -43,21 +43,21 @@ class Book extends PrintEditionItem {
 
 class NovelBook extends Book {
   constructor(author, name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount, author);
+    super(author, name, releaseDate, pagesCount);
     this.type = 'novel';
   }
 }
 
 class FantasticBook extends Book {
-  constructor(author, name, releaseDate, pagesCount, author) {
-    super(name, releaseDate, pagesCount, author);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = 'fantastic';
   }
 }
 
 class DetectiveBook extends Book {
-  constructor(author, name, releaseDate, pagesCount, author) {
-    super(name, releaseDate, pagesCount, author);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = 'detective';
   }
 }
