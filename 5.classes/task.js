c90a5ed909b21d864e6c9bae51f4008a34b390d0
Irina Agliullina <Agliullina2006@yandex.ru>
2023-03-27 15:21:00 +0400

    Задача 1, методы fix, get, set

---
+++ 5.classes/task.js
@@ -0,0 +1,30 @@
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+  
+  fix() {
+    this.state = this.state * 1.5;
+  }
+
+  set state(state) {
+    if (state < 0) {
+      this._state = 0;
+    } else if (state > 100) {
+      this._state = 100;
+    } else {
+      this._state = state;
+    }
+  }
+
+  get state() {
+    return this._state;
+  }
+
+  
+  
+}
